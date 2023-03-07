baa60db90dbcaf3474b6d433142e44c01eac1725
Giadagenovesi <giada.g@outlook.it>
2023-03-07 13:24:30 +0100

    Add project scaffolding

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Chiedere all'utente il suo nome, il suo cognome e il suo colore preferito e generare una password sicura
+
+
+// Chiedere il nome dell'utente
+
+
+// Chiedere il cognome dell'utente
+
+// Chiedere il colore preferito dell'utente
+
+
+// Generare la password: unire nome, cognome, colore preferito dell'utente e numero 21
+
+
+// Preparare il messaggio con il risultato
+
+
+// Stampare il messaggio
