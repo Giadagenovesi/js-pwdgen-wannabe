3b89c36a9f364c7f56dfc256efe71dea419ed45a
Giadagenovesi <giada.g@outlook.it>
2023-03-07 13:56:01 +0100

    Generated password

--- js/script.js
+++ js/script.js
@@ -2,17 +2,25 @@
 
 
 // Chiedere il nome dell'utente
-
+const userName = prompt ("Come ti chiami?");
+console.log(userName, typeof userName);
 
 // Chiedere il cognome dell'utente
+const userSurname = prompt ("Qual è il tuo cognome?");
+console.log(userSurname, typeof userSurname);
 
 // Chiedere il colore preferito dell'utente
-
+const userFavoriteColor = prompt ("Qual è il tuo colore preferito?");
+console.log(userFavoriteColor, typeof userFavoriteColor);
 
 // Generare la password: unire nome, cognome, colore preferito dell'utente e numero 21
+const password = `${userName}${userSurname}${userFavoriteColor}21`;
+console.log(password, typeof password);
 
 
 // Preparare il messaggio con il risultato
-
+const message = `La tua password è ${password}`;
+console.log(message, typeof message);
 
 // Stampare il messaggio
+document.getElementById('generated-pw').innerHTML = message;
